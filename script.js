ae138e592b722a93cdae19288d6fad46baaf33bd
tsv-stacks <tsvirdi@yahoo.co.uk>
2022-11-20 11:46:49 +0000

    added dark mode button

--- script.js
+++ script.js
@@ -3,3 +3,13 @@
 // checkbox.addEventListener('change', () => {
 //     document.body.classList.toggle('dark');
 // })
+
+let btnText = document.getElementById('btn-text')
+
+function darktolightbtn() {
+    if (btnText.innerText === "Dark Mode") {
+        btnText.innerText = "Light Mode"
+    } else if (btnText.innerText = "Light Mode") {
+        btnText.innerText = "Dark Mode"
+    }
+}
